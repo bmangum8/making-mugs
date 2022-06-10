bfd5732de5216e22aee374171ace31ba4a40ebbc
Brittany Mangum <brittanyturner8@gmail.com>
2022-06-10 15:56:31 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,10 @@
+
+const clay = [ "Clay", "Clay", "Clay", "Clay" ]
+const toFireInKiln = [] //Blank array will contain the copies
+
+for (const cups of clay) {
+   const mug = `${cups} coffee mug`
+   toFireInKiln.push(mug)
+}
+
+console.log(toFireInKiln)
